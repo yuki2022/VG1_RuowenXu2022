63664dc60395b65e05b24db5d2a939500a0d087d
Ruowen Xu <ruowen@wustl.edu>
2022-11-08 02:54:42 -0600

    Q7 Complete

--- Assets/Code/Q3/Target.cs
+++ Assets/Code/Q3/Target.cs
@@ -9,6 +9,8 @@ namespace Platformer
         {
             if (other.gameObject.GetComponent<Projectile>())
             {
+                PlayerController.instance.score++;
+                PlayerPrefs.SetInt("Score", PlayerController.instance.score);
                 Destroy(gameObject);
             }
         }
